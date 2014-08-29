8ea595eb95d247abf87ac9aafe542a444b5dfbc5
Matt Glaman <nmd.matt@gmail.com>
2014-08-29 15:26:57 -0500

    Refactor drupal.aliases.drushrc.php and close #14

--- drupal.aliases.drushrc.php
+++ drupal.aliases.drushrc.php
@@ -5,21 +5,82 @@
  * sites.
  */
 
-// We strip 3 for ., .., .DS_Store (so, this is Mac specific.)
+// Home directory, used to get Vagrant insecure private key.
 $home = drush_server_home();
-// Modify below as needed. I have repo cloned to ~/Drupal.
-$sites_path = $home . '/Drupal/sites';
-$clients = array_slice(scandir($sites_path), 3);
 
-foreach ($clients as $key => $name) {
-  $aliases[$name] = array(
-    'uri' => $name . '.dev',
-    'remote-host' => '10.22.22.10',
-    'remote-user' => 'vagrant',
-    'ssh-options' => '-p 22 -i ' . $home . '/.vagrant.d/insecure_private_key',
-    'root' => '/var/www/' . $name,
-    'path-aliases' => array(
-       '%files' => 'sites/default/files',
-     ),
-  );
-}
+$aliases['commerce2x'] = array(
+  'uri' => 'commerce2x.dev',
+  'remote-host' => '10.22.22.10',
+  'remote-user' => 'vagrant',
+  'ssh-options' => '-p 22 -i ' . $home . '/.vagrant.d/insecure_private_key',
+  'root' => '/var/www/commerce-2.x',
+  'path-aliases' => array(
+     '%files' => 'sites/commerce2x.dev/files',
+   ),
+);
+
+$aliases['commercedeploy'] = array(
+  'uri' => 'commercedeploy.dev',
+  'remote-host' => '10.22.22.10',
+  'remote-user' => 'vagrant',
+  'ssh-options' => '-p 22 -i ' . $home . '/.vagrant.d/insecure_private_key',
+  'root' => '/var/www/commerce_deploy',
+  'path-aliases' => array(
+     '%files' => 'sites/commercedeploy.dev/files',
+   ),
+);
+
+$aliases['commercekickstart'] = array(
+  'uri' => 'commercekickstart.dev',
+  'remote-host' => '10.22.22.10',
+  'remote-user' => 'vagrant',
+  'ssh-options' => '-p 22 -i ' . $home . '/.vagrant.d/insecure_private_key',
+  'root' => '/var/www/commerce_kickstart',
+  'path-aliases' => array(
+     '%files' => 'sites/commercekickstart.dev/files',
+   ),
+);
+
+$aliases['drupal7'] = array(
+  'uri' => 'drupal7.dev',
+  'remote-host' => '10.22.22.10',
+  'remote-user' => 'vagrant',
+  'ssh-options' => '-p 22 -i ' . $home . '/.vagrant.d/insecure_private_key',,
+  'root' => '/var/www/drupal-7',
+  'path-aliases' => array(
+     '%files' => 'sites/drupal7.dev/files',
+   ),
+);
+
+$aliases['drupal8'] = array(
+  'uri' => 'drupal8.dev',
+  'remote-host' => '10.22.22.10',
+  'remote-user' => 'vagrant',
+  'ssh-options' => '-p 22 -i ' . $home . '/.vagrant.d/insecure_private_key',,
+  'root' => '/var/www/drupal-8',
+  'path-aliases' => array(
+     '%files' => 'sites/default/files',
+   ),
+);
+
+$aliases['openatrium'] = array(
+  'uri' => 'openatrium.dev',
+  'remote-host' => '10.22.22.10',
+  'remote-user' => 'vagrant',
+  'ssh-options' => '-p 22 -i ' . $home . '/.vagrant.d/insecure_private_key',,
+  'root' => '/var/www/openatrium',
+  'path-aliases' => array(
+     '%files' => 'sites/openatrium.dev/files',
+   ),
+);
+
+$aliases['panopoly'] = array(
+  'uri' => 'panopoly.dev',
+  'remote-host' => '10.22.22.10',
+  'remote-user' => 'vagrant',
+  'ssh-options' => '-p 22 -i ' . $home . '/.vagrant.d/insecure_private_key',,
+  'root' => '/var/www/panopoly',
+  'path-aliases' => array(
+     '%files' => 'sites/panopoly.dev/files',
+   ),
+);
