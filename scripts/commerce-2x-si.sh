c3a1f715fa69da47f66aa490434811738050543e
Matt Glaman <nmd.matt@gmail.com>
2014-08-29 22:41:35 -0500

    Update Commerce 2.x install script to enable specific modules, update Composer.

--- scripts/commerce-2x-si.sh
+++ scripts/commerce-2x-si.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
 drush @drupal.commerce2x si standard --debug --site-name="Commerce2x.dev" --sites-subdir="commerce2x.dev" --db-url="mysql://commerce2x:commerce2x@localhost/commerce2x" --account-pass="admin" -y
+# Enable certain modules
+drush @drupal.commerce2x en devel simpletest -y
+# Commerce module (minus Price..requires a library and composer is still janked)
+drush @drupal.commerce2x en commerce commerce_order commerce_product
+# Composer update
+vagrant ssh -c "cd /var/www/drupal-8/sites/default/files/composer && composer update"
