139a304cda0c33f5e63ab927d54cbac5699a9174
Matt Glaman <nmd.matt@gmail.com>
2014-08-30 12:36:28 -0500

    Add a script to provision the VM's hosts, that way Selenium knows where to go

---
+++ puphpet/files/exec-once/provision-hosts.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# @todo: Kill the n00bish, gotta be smarter way to do this.
+sudo echo "127.0.1.1  commerce2x.dev commerce2x" >> /etc/hosts
+sudo echo "127.0.1.1  commercedeploy.dev commercedeploy" >> /etc/hosts
+sudo echo "127.0.1.1  commercekickstart.dev commercekickstart" >> /etc/hosts
+sudo echo "127.0.1.1  drupal7.dev drupal7" >> /etc/hosts
+sudo echo "127.0.1.1  drupal8.dev drupal8" >> /etc/hosts
+sudo echo "127.0.1.1  openatrium.dev openatrium" >> /etc/hosts
+sudo echo "127.0.1.1  panopoly.dev panopoly" >> /etc/hosts
+
