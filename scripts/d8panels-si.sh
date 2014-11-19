6d706cf798a1a645bf584aaa2b07d9e8eeaa58c4
Matt Glaman <nmd.matt@gmail.com>
2014-11-18 21:15:28 -0600

    One more tweak to build/install because of misnaming of Panels module.

--- scripts/d8panels-si.sh
+++ scripts/d8panels-si.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 drush @drupal.d8panels si standard --debug --site-name="D8Panels.dev" --sites-subdir="d8panels.dev" --db-url="mysql://d8panels:d8panels@localhost/d8panels" --account-pass="admin" -y
 # Enable certain modules
-drush @drupal.d8panels en page_manager layout_plugin d8panels -y
+drush @drupal.d8panels en page_manager layout_plugin panels -y
