9279d6cca6ad46c0009725a869c56464072ebf66
Matt Glaman <nmd.matt@gmail.com>
2014-11-18 20:56:43 -0600

    For some reason setup-selenium.sh is running more than it should, causing errors. Check if directory exists first.

--- puphpet/files/exec-once/setup-selenium.sh
+++ puphpet/files/exec-once/setup-selenium.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-sudo mkdir /var/selenium && cd /var/selenium
-wget http://selenium-release.storage.googleapis.com/2.40/selenium-server-standalone-2.40.0.jar
-# @todo: The following could be moved into Puppet, I'm assuming?
-sudo apt-get update
-sudo apt-get install openjdk-7-jre-headless -y
-sudo apt-get install firefox -y
-sudo apt-get install xvfb -y
+if [ -d "/var/selenium" ]; then
+  sudo mkdir /var/selenium && cd /var/selenium
+  wget http://selenium-release.storage.googleapis.com/2.40/selenium-server-standalone-2.40.0.jar
+  # @todo: The following could be moved into Puppet, I'm assuming?
+  sudo apt-get update
+  sudo apt-get install openjdk-7-jre-headless -y
+  sudo apt-get install firefox -y
+  sudo apt-get install xvfb -y
+fi
